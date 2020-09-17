27ddfaf7487ad7d1f35a106d0a0cb1c4f2147b19
Matthias Buchholz <mbuchholz@babbel.com>
2020-09-17 12:46:08 +0200

    backend default port: 5000

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   public: process.env.STATIC_FILES_DIR || path.resolve(__dirname, '../static'),
-  port: Number(process.env.STATIC_HTTP_PORT || '3001'),
+  port: Number(process.env.STATIC_HTTP_PORT || '5000'),
 };
